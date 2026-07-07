bbd4b4ac4861e688edeeddd3d077a4b8bb44a3ed
vanshika <vanshikalothe7@gmail.com>
2026-07-07 18:50:34 +0530

    Getting Started with Project1

---
+++ JobPortal/JobPortal/src/test/java/com/S2p/JobPortal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S2p.JobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
